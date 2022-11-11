74bbff6424e49f27d30e4d2942e99f63dfc43ea6
Maxxchop <4mu@bk.ru>
2022-11-11 13:52:50 +0300

    all views added

--- ads/views.py
+++ ads/views.py
@@ -1,11 +1,117 @@
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView
 
+from ads.models import Ad, Category
 from my_csv import csv_to_json
 
 
 def root(request):
 
-#    return JsonResponse({"status": "ok"}, status=200)
-    return JsonResponse(csv_to_json('./ads.csv'), safe=False, status=200)
+    return JsonResponse({"status": "ok"}, status=200)
+#    return JsonResponse(csv_to_json('./ads.csv'), safe=False, status=200)
 
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AdView(View):
+    def get(self, request):
+        ads = Ad.objects.all()
+
+        response = []
+        for ad in ads:
+            response.append({
+                "id": ad.id,
+                "name": ad.name,
+                "author": ad.author,
+                "price": ad.price,
+                "description": ad.description,
+                "address": ad.address,
+                "is_published": ad.is_published
+            })
+        return JsonResponse(response, safe=False)
+
+    def post(self, request):
+        ad_data = json.loads(request.body)
+
+        ad = Ad()
+        ad.name = ad_data["name"]
+        ad.author = ad_data["author"]
+        ad.price = ad_data["price"]
+        ad.description = ad_data["description"]
+        ad.address = ad_data["address"]
+        ad.is_published = ad_data["is_published"]
+
+        ad.save()
+
+        return JsonResponse({
+            "id": ad.id,
+            "name": ad.name,
+            "author": ad.author,
+            "price": ad.price,
+            "description": ad.description,
+            "address": ad.address,
+            "is_published": ad.is_published
+        })
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CatView(View):
+
+    def get(self, request):
+        cats = Category.objects.all()
+
+        response = []
+        for cat in cats:
+            response.append({
+                "id": cat.id,
+                "name": cat.name
+            })
+
+        return JsonResponse(response, safe=False)
+
+    def post(self, request):
+        cat_data = json.loads(request.body)
+
+        cat = Category()
+        cat.name = cat_data["name"]
+
+        cat.save()
+
+        return JsonResponse({
+            "id": cat.id,
+            "name": cat.name
+        })
+
+
+class AdDetailView(DetailView):
+    model = Ad
+
+    def get(self, *args, **kwargs):
+        ad = self.get_object()
+
+        return JsonResponse({
+            "id": ad.id,
+            "name": ad.name,
+            "author": ad.author,
+            "price": ad.price,
+            "description": ad.description,
+            "address": ad.address,
+            "is_published": ad.is_published
+        })
+
+
+class CatDetailView(DetailView):
+    model = Category
+
+    def get(self, *args, **kwargs):
+        cat = self.get_object()
+
+        return JsonResponse({
+            "id": cat.id,
+            "name": cat.name
+        })
--- skypro_django_hw1/urls.py
+++ skypro_django_hw1/urls.py
@@ -21,4 +21,8 @@ from ads import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.root),
+    path("ad/", views.AdView.as_view()),
+    path("ad/<int:pk>", views.AdDetailView.as_view()),
+    path("cat/", views.CatView.as_view()),
+    path("cat/<int:pk>", views.CatDetailView.as_view()),
 ]
